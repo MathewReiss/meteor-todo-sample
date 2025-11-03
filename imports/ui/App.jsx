23418f848bd3ef8c3152707a48188107ef35ef4b
Mat Reiss <mathew_reiss@alumni.brown.edu>
2025-11-03 18:24:10 -0500

    Copy from local

---
+++ imports/ui/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useTracker, useSubscribe } from "meteor/react-meteor-data";
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { Task } from "./Task";
+import { TaskForm } from "./TaskForm";
+
+const tasks = [
+  {_id: 1, text: 'First Task'},
+  {_id: 2, text: 'Second Task'},
+  {_id: 3, text: 'Third Task'},
+];
+
+export const App = () => {
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  const handleToggleChecked = ({ _id, isChecked }) => Meteor.callAsync("tasks.toggleChecked", { _id, isChecked });
+  const handleDelete = ({ _id }) => Meteor.callAsync("tasks.delete", { _id });
+
+  const isLoading = useSubscribe("tasks");
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const tasks = useTracker(() => TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }).fetch());
+
+  const pendingTasksCount = useTracker(() =>
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+
+  if (isLoading()) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>📝️ To Do List {pendingTasksTitle}</h1>
+          </div>
+        </div>
+      </header>
+      <div className="main">
+        <TaskForm />
+
+        <div className="filter">
+          <button onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? 'Show All' : 'Hide Completed'}
+          </button>
+        </div>
+
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={handleToggleChecked}
+              onDeleteClick={handleDelete}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
